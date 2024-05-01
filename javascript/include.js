d08331b21166548e1ef796cca4d1f2109ae603b5
AirLi1507 <167706999+AirLi1507@users.noreply.github.com>
2024-05-02 00:54:54 +0800

    Fixing .php redirecting

--- javascript/include.js
+++ javascript/include.js
@@ -2,25 +2,25 @@ var listDiv = `
 <div class="content-list">
     <div class="vertical">
         <li class="content-list-title"><h3 class="text1">目錄</h3></li>
-        <li class="content-list">1.<a href="section1.php#section1" class="section-link">前言</a></li>
-        <li class="content-list-sub">> 1.1 <a href="section1.php#sub-section1" class="section-link-sub">「樹木和人」專題研習課程的意義是甚麼?為甚麼本校要將「樹木和人」推展至校內課
+        <li class="content-list">1.<a href="section1.html#section1" class="section-link">前言</a></li>
+        <li class="content-list-sub">> 1.1 <a href="section1.html#sub-section1" class="section-link-sub">「樹木和人」專題研習課程的意義是甚麼?為甚麼本校要將「樹木和人」推展至校內課
             程?</a></li>
-        <li class="content-list-sub">> 1.2 <a href="section1.php#sub-section2" class="section-link-sub">在「樹木和人」中二級課程你學過甚麼內容(地理和科學科)?</a></li>
-        <li class="content-list-sub">> 1.3 <a href="section1.php#sub-section3" class="section-link-sub">在「樹木和人」大棠考察活動日中,學過甚麼知識和技能?</a></li>
-        <li class="content-list">2.<a href="section2.php#section2" class="section-link">大棠的人為破壞及保育的歷史</a></li>
-        <li class="content-list-sub">2.1 <a href="section2.php#sub-section4" class="section-link-sub">在大棠中曾經受人為破壞的歷史</a></li>
-        <li class="content-list-sub">2.2 <a href="section2.php#sub-section5" class="section-link-sub">補救方法</a></li>
-        <li class="content-list">3.<a href="section3.php#section3" class="section-link">植林</a></li>
-        <li class="content-list-sub">> 3.1 <a href="section3.php#sub-section6" class="section-link-sub">大棠植林和大棠樹木研習徑的源起。</a></li>
-        <li class="content-list-sub">> 3.2 <a href="section3.php#sub-section7" class="section-link-sub">植林對樹木生態的演替過程有甚麼幫助</a></li>
-        <li class="content-list-sub">> 3.3 <a href="section3.php#sub-section8" class="section-link-sub">漁護署對大棠植林有甚麼角色?</a></li>
-        <li class="content-list-sub">> 3.4 <a href="section3.php#sub-section9" class="section-link-sub">大棠植林的發展階段。</a></li>
-        <li class="content-list-sub">> 3.5 <a href="section3.php#sub-section10" class="section-link-sub">大棠苗圃是甚麼,對於植林有甚麼功用?</a></li>
-        <li class="content-list">4.<a href="section4.php#section4" class="section-link">原生樹和外來樹</a></li>
-        <li class="content-list-sub">> 4.1 <a href="section4.php#sub-section11" class="section-link-sub">原生樹和外來樹有甚麼分別?</a></li>
-        <li class="content-list-sub">> 4.2 <a href="section4.php#sub-section12" class="section-link-sub">比較原生樹和外來樹,以分析種植原生樹和外來樹有甚麼優點或缺點?對環境有甚麼正面或負面的影響?</a></li>
-        <li class="content-list-sub">> 4.3 <a href="section4.php#sub-section13" class="section-link-sub">一種原生樹和一種外來樹的例子。其特徵外該樹木品種對於環境及植林的特別功能。</a></li>
-        <li class="content-list">5.<a href="section5.php#section5" class="section-link">反思 + 資料來源</a></li>
+        <li class="content-list-sub">> 1.2 <a href="section1.html#sub-section2" class="section-link-sub">在「樹木和人」中二級課程你學過甚麼內容(地理和科學科)?</a></li>
+        <li class="content-list-sub">> 1.3 <a href="section1.html#sub-section3" class="section-link-sub">在「樹木和人」大棠考察活動日中,學過甚麼知識和技能?</a></li>
+        <li class="content-list">2.<a href="section2.html#section2" class="section-link">大棠的人為破壞及保育的歷史</a></li>
+        <li class="content-list-sub">2.1 <a href="section2.html#sub-section4" class="section-link-sub">在大棠中曾經受人為破壞的歷史</a></li>
+        <li class="content-list-sub">2.2 <a href="section2.html#sub-section5" class="section-link-sub">補救方法</a></li>
+        <li class="content-list">3.<a href="section3.html#section3" class="section-link">植林</a></li>
+        <li class="content-list-sub">> 3.1 <a href="section3.html#sub-section6" class="section-link-sub">大棠植林和大棠樹木研習徑的源起。</a></li>
+        <li class="content-list-sub">> 3.2 <a href="section3.html#sub-section7" class="section-link-sub">植林對樹木生態的演替過程有甚麼幫助</a></li>
+        <li class="content-list-sub">> 3.3 <a href="section3.html#sub-section8" class="section-link-sub">漁護署對大棠植林有甚麼角色?</a></li>
+        <li class="content-list-sub">> 3.4 <a href="section3.html#sub-section9" class="section-link-sub">大棠植林的發展階段。</a></li>
+        <li class="content-list-sub">> 3.5 <a href="section3.html#sub-section10" class="section-link-sub">大棠苗圃是甚麼,對於植林有甚麼功用?</a></li>
+        <li class="content-list">4.<a href="section4.html#section4" class="section-link">原生樹和外來樹</a></li>
+        <li class="content-list-sub">> 4.1 <a href="section4.html#sub-section11" class="section-link-sub">原生樹和外來樹有甚麼分別?</a></li>
+        <li class="content-list-sub">> 4.2 <a href="section4.html#sub-section12" class="section-link-sub">比較原生樹和外來樹,以分析種植原生樹和外來樹有甚麼優點或缺點?對環境有甚麼正面或負面的影響?</a></li>
+        <li class="content-list-sub">> 4.3 <a href="section4.html#sub-section13" class="section-link-sub">一種原生樹和一種外來樹的例子。其特徵外該樹木品種對於環境及植林的特別功能。</a></li>
+        <li class="content-list">5.<a href="section5.html#section5" class="section-link">反思 + 資料來源</a></li>
         <li class="content-list" style="list-style: none; height: 20px;"></li>
 </div>
 </div>
